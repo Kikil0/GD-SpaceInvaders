fb71298441194efbc29cfc9b0c90b9c816abd5dd
Kikil0 <osvaldo.gonzalez@gadz.org>
2023-10-24 12:48:42 +0200

    Enemy script
    - Defined width and height parameters in the enemy
    
    ENEMY_GROUP
    - Added Spawner configuration constants and variables
    - Improved clarity on the spawning logic of the enemies.
    - created a spawn_enemy function
    - Added Enemy Movement to the functionality, as well as the "Speed Up" mechanic using time-decrements.

--- Scenes/Enemy.gd
+++ Scenes/Enemy.gd
@@ -1,17 +1,29 @@
 extends Area2D
-var width
-var height
+
+class_name Enemy
+signal died
+
+@export var width : int
+@export var height : int
+
+# Timer to delay the queue_free for the enemy
+var destroy_timer : Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	width = get_node("CollisionShape2D").shape.get_size()[0]
-	height = get_node("CollisionShape2D").shape.get_size()[1]
+	
+	##Create a Shot Timer
+	destroy_timer = Timer.new()
+	destroy_timer.one_shot = true
+	destroy_timer.wait_time = 0.1
+	add_child(destroy_timer)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _on_area_entered(area):
-	if area.name == "Projectile":
+	if area is Projectile:
 		area.queue_free()
+		died.emit()
 		queue_free()
--- Scenes/enemy_group.gd
+++ Scenes/enemy_group.gd
@@ -1,24 +1,67 @@
 extends Node2D
 @export var MobScene: PackedScene
-@export var padding = 2
+
+#Spawner Configuration
+const ROWS = 11
+const COLUMNS = 4
+const HORIZONTAL_SPACING = 50
+const VERTICAL_SPACING = 32
+const ENEMY_HEIGHT = 8
+const START_Y_POSITION = 0
+const ENEMY_X_INCREMENT = 5
+const ENEMY_Y_INCREMENT = 10
+
+var enemy_count = COLUMNS*ROWS
+var movement_direction = 1
+var time_decrement
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	## Arrange the mobs in an array
-	for i in range(4):
-		
-		for j in range(11):
-			#Create a new instance of the mob
-			var mob = MobScene.instantiate()
+#	## Arrange the mobs in an array
+	var enemy = MobScene.instantiate() as Enemy
+	for row in ROWS:
+		var row_width = (COLUMNS * enemy.width * 3) + ((COLUMNS-1)*HORIZONTAL_SPACING)
+		var start_x = (position.x - row_width)
+	
+		for col in COLUMNS:
+			var x = start_x + (col * enemy.width * 3) + (col * HORIZONTAL_SPACING)
+			var y = START_Y_POSITION + (row * enemy.height) + (row * VERTICAL_SPACING)
 			
-			#Create a column of mobs
-			mob.position = Vector2(i*(8*6 + 6*padding), -j * (6*6 + 6*padding))
-			print(mob.position)
-			add_child(mob)
-			
+			var spawn_position = Vector2(x, y)
+			enemy.position = spawn_position
+			print(enemy.position)
+			spawn_enemy(enemy, spawn_position)
+	
+	$MovementTimer.start()
+	$MovementTimer.connect("timeout", move_enemies)
+	time_decrement = $MovementTimer.wait_time/enemy_count
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+#Spawn Enemy Function
+func spawn_enemy(enemy_scene, spawn_position: Vector2):
+	var enemy = MobScene.instantiate() as Enemy
+	enemy.position = spawn_position
+	add_child(enemy)
+	enemy.connect("died", _on_enemy_died)
+	print(enemy.position)
+
+func move_enemies():
+	position.x += ENEMY_X_INCREMENT*movement_direction
+
+func _on_wall_left_area_entered(area):
+	if (movement_direction == -1):
+		position.y += ENEMY_Y_INCREMENT
+		movement_direction = -1 * movement_direction
+
+func _on_wall_right_area_entered(area):
+	if (movement_direction == 1):
+		position.y += ENEMY_Y_INCREMENT
+		movement_direction = -1 * movement_direction
+
+func _on_enemy_died():
+	print("died")
+	enemy_count -= 1
+	$MovementTimer.wait_time -= time_decrement
+
 func _process(delta):
-	pass
-
+	print(str($MovementTimer.wait_time))
 
